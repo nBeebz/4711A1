ccbcc5f3bbb0d128e4ae2bb7153ce0c678074200
Nav Bhatti <bhatti.nav@gmail.com>
2015-03-08 18:22:42 -0700

    resolved dependancies
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

--- application/views/timer.php
+++ application/views/timer.php
@@ -1,6 +1,6 @@
 <table>
 <tr><td colspan=2><h1>0:00:00</h1></td></tr>
-<tr><td><button style="width:100%">Start</button></td>
-<td><button style="width:100%">Stop</button></td></tr>
+<tr><td><button style="width:100%" id="start">Start</button></td>
+<td><button style="width:100%" id="stop">Stop</button></td></tr>
 </table>
 <br/>
