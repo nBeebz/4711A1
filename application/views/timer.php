6ab5cff85d71009aae112786a9aa98592fd94624
Nav Bhatti <bhatti.nav@gmail.com>
2015-03-08 18:48:49 -0700

    implemented timer
    Signed-off-by: Nav Bhatti <bhatti.nav@gmail.com>

--- application/views/timer.php
+++ application/views/timer.php
@@ -1,6 +1,19 @@
 <table>
-<tr><td colspan=2><h1>0:00:00</h1></td></tr>
-<tr><td><button style="width:100%" id="start">Start</button></td>
-<td><button style="width:100%" id="stop">Stop</button></td></tr>
+	<tr>
+		<td colspan=2>
+		<h1>
+			<span id="hours">0</span>h
+			<span id="minutes">0</span>m
+			<span id="seconds">0</span>.<span id="msecs">0</span>s
+		</h1>
+		</td>
+	</tr>
+	<tr>
+		<td><button style="width:100%" id="start">Start</button></td>
+		<td><button style="width:100%" id="pause">Pause</button></td>
+	</tr>
+	<tr>
+		<td colspan=2><button style="width:100%" id="stop">Stop</button></td>
+	</tr>
 </table>
 <br/>
